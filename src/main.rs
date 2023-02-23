a102b6145364191acecfb29310685870f9a488b5
Imad Jerra <smartshader@gmail.com>
2023-02-22 22:07:07 -0500

    add visitor list

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,41 @@ fn what_is_your_name() -> String {
     your_name.trim().to_lowercase()
 }
 
+#[derive(Debug)]
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
 fn main() {
+    let visitor_list = [
+        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+        Visitor::new("steve", "Hello Steve, Your milk is in the fridge."),
+        Visitor::new("fred", "Wow, who invited Fred?"),
+    ];
+
     println!("Hello, what's your name?");
+
     let name = what_is_your_name();
-    println!("Hello, {:?}", name)
+
+    let known_visitor =
+        visitor_list.iter().find(|visitor| visitor.name == name);
+
+    match known_visitor {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("You are not on the Visitor list. Please leave."),
+    }
 }
